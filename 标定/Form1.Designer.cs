962e310a9c5aa9d82da6883736e5c7653e000c93
Administrator <Administrator@ZL-COM>
2024-08-16 16:55:09 +0800

    添加项目文件。

---
+++ 标定/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace 标定
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hWindowControl1 = new HalconDotNet.HWindowControl();
+            this.采集图像 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // hWindowControl1
+            // 
+            this.hWindowControl1.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl1.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl1.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hWindowControl1.Location = new System.Drawing.Point(11, 11);
+            this.hWindowControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.hWindowControl1.Name = "hWindowControl1";
+            this.hWindowControl1.Size = new System.Drawing.Size(1113, 662);
+            this.hWindowControl1.TabIndex = 15;
+            this.hWindowControl1.WindowSize = new System.Drawing.Size(1113, 662);
+            // 
+            // 采集图像
+            // 
+            this.采集图像.Location = new System.Drawing.Point(1224, 60);
+            this.采集图像.Name = "采集图像";
+            this.采集图像.Size = new System.Drawing.Size(159, 38);
+            this.采集图像.TabIndex = 16;
+            this.采集图像.Text = "采集图像";
+            this.采集图像.UseVisualStyleBackColor = true;
+            this.采集图像.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1224, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 38);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "执行标定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1224, 217);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 21);
+            this.textBox1.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1224, 244);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(159, 21);
+            this.textBox2.TabIndex = 19;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1224, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 38);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "标定验证";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1224, 299);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(73, 21);
+            this.textBox3.TabIndex = 21;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1303, 299);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(80, 21);
+            this.textBox4.TabIndex = 22;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1303, 370);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(80, 21);
+            this.textBox5.TabIndex = 24;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(1224, 370);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(73, 21);
+            this.textBox6.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1442, 684);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.采集图像);
+            this.Controls.Add(this.hWindowControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private HalconDotNet.HWindowControl hWindowControl1;
+        private System.Windows.Forms.Button 采集图像;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ 标定/Form1.cs
@@ -0,0 +1,146 @@
+﻿using HalconDotNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace 标定
+{
+    public partial class Form1 : Form
+    {
+        
+        private static HWindow hwindow;
+        public HTuple ImagePath = new HTuple();
+        HTuple ImageHeight = null, ImageWidth = null;
+        private HObject getImage = new HObject();
+        private HObject getImage1 = new HObject();
+        HObject ho_Image, ho_GrayImage, ho_Regions;
+        HObject ho_WiresFilled, ho_Balls, ho_SingleBalls, ho_IntermediateBalls;
+        HObject ho_Circle;
+
+        // Local control variables 
+
+        HTuple hv_Row = new HTuple(), hv_Column = new HTuple();
+        HTuple hv_Radius = new HTuple(), hv_WorldRow = new HTuple();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox3.Text);
+            double b = Convert.ToDouble(textBox4.Text);
+            HOperatorSet.AffineTransPoint2d(hv_HomMat2D1, a, b, out hv_Qx, out hv_Qy);
+            textBox5.Text = hv_Qx.ToString();
+            textBox6.Text = hv_Qy.ToString();
+        }
+
+        HTuple hv_WorldCol = new HTuple(), hv_Index = new HTuple();
+        HTuple hv_HomMat2D1 = new HTuple(), hv_SerializedItemHandle = new HTuple();
+        HTuple hv_FileHandle = new HTuple(), hv_Qx = new HTuple();
+        HTuple hv_Qy = new HTuple();
+        // Initialize local and output iconic variables 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+        HOperatorSet.GenEmptyObj(out ho_Image);
+            HOperatorSet.GenEmptyObj(out ho_GrayImage);
+            HOperatorSet.GenEmptyObj(out ho_Regions);
+            HOperatorSet.GenEmptyObj(out ho_WiresFilled);
+            HOperatorSet.GenEmptyObj(out ho_Balls);
+            HOperatorSet.GenEmptyObj(out ho_SingleBalls);
+            HOperatorSet.GenEmptyObj(out ho_IntermediateBalls);
+            HOperatorSet.GenEmptyObj(out ho_Circle);
+            HOperatorSet.Rgb1ToGray(getImage, out getImage1);
+            HOperatorSet.Threshold(getImage1, out ho_Regions, 13, 71);
+            HOperatorSet.DispObj(getImage1, hwindow);
+            //填充缺失
+            ho_WiresFilled.Dispose();
+            HOperatorSet.FillUpShape(ho_Regions, out ho_WiresFilled, "area", 1, 100);
+
+            //开操作 腐蚀和膨胀的结合，即先腐蚀后膨胀
+            ho_Balls.Dispose();
+            HOperatorSet.OpeningCircle(ho_WiresFilled, out ho_Balls, 15.5);
+
+            ho_SingleBalls.Dispose();
+            HOperatorSet.Connection(ho_Balls, out ho_SingleBalls);
+
+            //找到具有目标特征的形状，这边填写的参数是 ‘circularity’ ，就是类圆的图形；
+            ho_IntermediateBalls.Dispose();
+            HOperatorSet.SelectShape(ho_SingleBalls, out ho_IntermediateBalls, "circularity",
+                "and", 0.85, 1.0);
+
+            //确定这些圆形区域的最小外接圆，并将输出的坐标和半径做处理后输出
+            hv_Row.Dispose(); hv_Column.Dispose(); hv_Radius.Dispose();
+            HOperatorSet.SmallestCircle(ho_SingleBalls, out hv_Row, out hv_Column, out hv_Radius);
+
+            //生成 虚拟机械坐标 行往下偏移80
+            hv_WorldRow.Dispose();
+            hv_WorldRow = new HTuple();
+            hv_WorldCol.Dispose();
+            hv_WorldCol = new HTuple();
+
+            for (hv_Index = 0; (int)hv_Index <= (int)((new HTuple(hv_Row.TupleLength())) - 1); hv_Index = (int)hv_Index + 1)
+            {
+                if (hv_WorldRow == null)
+                    hv_WorldRow = new HTuple();
+                hv_WorldRow[hv_Index] = (hv_Row.TupleSelect(hv_Index)) + 80;
+                if (hv_WorldCol == null)
+                    hv_WorldCol = new HTuple();
+                hv_WorldCol[hv_Index] = hv_Column.TupleSelect(hv_Index);
+                using (HDevDisposeHelper dh = new HDevDisposeHelper())
+                {
+                    ho_Circle.Dispose();
+                    HOperatorSet.GenCircle(out ho_Circle, hv_WorldRow.TupleSelect(hv_Index), hv_WorldCol.TupleSelect(
+                        hv_Index), 10);
+                }
+            }
+
+            //生成标定
+            hv_HomMat2D1.Dispose();
+            HOperatorSet.VectorToHomMat2d(hv_Row, hv_Column, hv_WorldRow, hv_WorldCol, out hv_HomMat2D1);
+            hv_SerializedItemHandle.Dispose();
+            HOperatorSet.SerializeHomMat2d(hv_HomMat2D1, out hv_SerializedItemHandle);
+            hv_FileHandle.Dispose();
+            HOperatorSet.OpenFile("C:/Users/Administrator/Desktop/111.mat", "output_binary",
+                out hv_FileHandle);
+            HOperatorSet.FwriteSerializedItem(hv_FileHandle, hv_SerializedItemHandle);
+            HOperatorSet.CloseFile(hv_FileHandle);
+            textBox1.Text = hv_Row.ToString();
+            textBox1.Text += hv_Column.ToString();
+            textBox2.Text = hv_WorldRow.ToString();
+            textBox2.Text += hv_WorldCol.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "JPEG文件|*.jpg*";
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.FilterIndex = 1;
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ImagePath = openFileDialog1.FileName;
+                HObject Image;
+                HOperatorSet.GenEmptyObj(out Image);
+                hwindow = hWindowControl1.HalconWindow;
+                hwindow.ClearWindow();
+                HOperatorSet.ReadImage(out getImage, ImagePath);
+                HOperatorSet.GetImageSize(getImage, out ImageWidth, out ImageHeight);
+                HOperatorSet.SetPart(hwindow, 0, 0, ImageHeight - 1, ImageWidth - 1);
+                HOperatorSet.DispObj(getImage, hwindow);
+            }
+        }
+    }
+}
